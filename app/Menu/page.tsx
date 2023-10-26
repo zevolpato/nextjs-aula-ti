cfd42972443c33ea5235dc984a4c7db46e2e7fba
zevolpato <zevolpato@outlook.com>
2023-10-26 18:07:07 +0000

    first

--- app/Menu/page.tsx
+++ app/Menu/page.tsx
@@ -1,13 +1,26 @@
+import { HiAcademicCap, HiOutlineHome, HiArrowRight,HiBugAnt } from 'react-icons/hi2'
+
+
+
 export default function Menu(){
+    
     return(
         <>
-        <ul>
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Portfolio</li>
-            <li>Produtos</li>
+        <div className="flex bg-amber-500 justify-between pl-20 pr-20 pt-2 pb-2">
+        <p className="inline-flex items-center">BUG<HiBugAnt size={60}/> ANT</p>
+        
+        <ul className="flex items-center">
+            <HiOutlineHome />
+            <li className="inline-flex pr-10 pl-2 items-center">  Menu</li>
+            <li className="inline-flex pr-10">Menu</li>
+            <HiAcademicCap />
+            <li className="inline-flex pr-10 pl-2 ">Menu</li>
+            <li className="inline-flex pr-10">Menu</li>
+            <li className="inline-flex"></li>
 
         </ul>
+        
+        </div>
         </>
 
     )
